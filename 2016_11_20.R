776e1d6d51fdc71c04ee133c2304ba14d28a54df
Pratik Gujral <pratikgujral@gmail.com>
2016-11-20 18:15:35 +0530

    Added code for multiple regression

--- 2016_11_20.R
+++ 2016_11_20.R
@@ -2,6 +2,7 @@
 working.directory <- "D:\\code\\R\\R-Practice\\"
 
 setwd(working.directory)
+
 # Linear Regression
 weight <- c(63,81,56,91,47,57,76,72,62,48)
 height <- c(151,174,138,186,128,136,179,163,152,131)
@@ -29,3 +30,24 @@ a <- data.frame(height = 170)
 weight.predicted <- predict(model, a)
 
 print(weight.predicted)
+########################################################
+# Multiple regression
+# Y = a + b1x1 + b2x2 + b3x3 + ...
+
+# Multile (linear) regression for miles/gallon vs (no. of cylinders, weight, displacement, horsepower) from mtcars
+model.multipleRegression  <- lm(formula = mpg~cyl+wt+disp+hp, data = mtcars)
+print(model.multipleRegression)
+
+# COefficients can be obtained using coef(...) or coefficients(...) 
+coef(model.multipleRegression)
+coef.intercept <- coef(model.multipleRegression)[1]
+coef.cyl <- coef(model.multipleRegression)[2]
+coef.wt <- coef(model.multipleRegression)[3]
+coef.disp <- coef(model.multipleRegression)[4]
+coef.hp <- coef(model.multipleRegression)[5]
+
+# Predicting for cyl=6, wt=2.5, disp=250, hp=100
+mpg.newData <- data.frame(cyl=6,wt=2.5,disp=250,hp=100)
+mpg.predicted <- predict(object = model.multipleRegression, newdata = mpg.newData)
+print(mpg.predicted)
+#########################################################
